29ad237c74c9b8cee534995fc03ad2e21bb16013
Abdallaaq97 <a.abuquboh@gmail.com>
2025-08-07 13:54:26 +0300

    Add .next/static/chunks/src_app_social-automation_page_tsx_a993527f._.js 1754563938856

---
+++ .next/static/chunks/src_app_social-automation_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_abd941d3._.js",
+  "static/chunks/node_modules_ccb771c3._.js"
+],
+    source: "dynamic"
+});
